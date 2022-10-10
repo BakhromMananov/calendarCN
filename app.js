f3fdcc6bae4a644fa27f453c8bf9276d4ae2a2d6
Bakhrom Mananov <39458044+BakhromMananov@users.noreply.github.com>
2022-10-10 18:25:47 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+const currentDate=document.querySelector('.current-date'),
+daysTag= document.querySelector('.days'),
+prevNextIcon=document.querySelectorAll('.icons span')
+
+let date= new Date(),
+currYear= date.getFullYear(),
+currMonth=date.getMonth() ;
+
+let months= ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const renderCalendar=() => {
+    //getting first date of month
+    let firstDayOfMonth= new Date(currYear, currMonth, 1).getDay(),
+    
+    lastDateOfMonth= new Date(currYear, currMonth + 1, 0).getDate(), 
+    lastDayOfMonth= new Date(currYear, currMonth, lastDateOfMonth).getDay(), 
+    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();  
+    let liTag="";
+
+    
+    for(let i=firstDayOfMonth; i > 0; i--) {
+        liTag +=`<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+    }
+
+    for(let i=1; i <=lastDateOfMonth; i++) {
+        let isToday= i === date.getDate() && currMonth=== new Date().getMonth() && currYear=== new Date().getFullYear() ?  "active" : ""
+       liTag +=`<li class="${isToday}">${i}</li>`; 
+    }
+
+    for(let i =lastDayOfMonth; i < 6; i ++  ) {
+        liTag +=`<li class="inactive">${i - lastDayOfMonth + 1}</li>`;
+    }
+
+    currentDate.innerText=`${months[currMonth]} ${currYear}`;
+    daysTag.innerHTML=liTag;
+
+}
+
+renderCalendar(); 
+
+prevNextIcon.forEach(icon => {
+    icon.addEventListener('click', () => {
+        currMonth= icon.id=== 'prev' ? currMonth - 1 : currMonth +1; 
+        if(currMonth<0 || currMonth > 11 ) {
+            date= new Date(currYear, currMonth); 
+            currYear= date.getFullYear(); 
+            currMonth= date.getMonth(); 
+        } else {
+            date = new Date(); 
+        }
+        renderCalendar(); 
+    })
+})
+
+
+// https://www.youtube.com/watch?v=Z1BGAivZRlE&list=PLpwngcHZlPadhRwryAXw3mJWX5KH3T5L3
